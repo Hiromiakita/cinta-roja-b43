11570e19e0435f6d041bdcf0de19823033c905ac
Hiromi Akita <hiromiakitao@gmail.com>
2020-07-21 18:11:05 -0500

    agregando carpeta de repaso

---
+++ 0_repaso/app.js
@@ -0,0 +1,12 @@
+console.log('esto es una prueba!!');
+const numero = 365;       // Number
+console.log(typeof numero);
+ 
+const dojo = "DEV.F"      // String
+console.log(typeof numero);
+
+const estoyVivo = true    // Boolean
+console.log(typeof estoyVivo);
+
+const articulos = null;   // Null
+console.log(typeof articulos);
