0ee89aab775c419bb4f13c82bc4cd08ee1aa5586
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2020-07-24 21:39:00 -0500

    ejercicios herencia

---
+++ 3_HERENCIA/ejercicios.js
@@ -0,0 +1,39 @@
+// ------------------------------------------------------------
+// 1. Hacer superclase Maestro y subclases Maestro de Física y 
+//    Maestro de Música y a cada uno asignarle su materia y 
+//    calcular su promedio de grupo a partir de calificaciones 
+//    (puedes usar arreglos). El maestro de física tiene un 
+//    atributo "antiguedad" que guarda un valor numerico, 
+//    mientras que el maestro de música tiene un atributo "edad"
+//    también guardando un valor numérico.
+// ------------------------------------------------------------
+
+// ------------------------------------------------------------
+// 2.- Crea una superclase llamada Electrodoméstico con las 
+//     siguientes características:
+//     a. Sus atributos son precio, color, consumoEnergetico
+//        y capacidad (peso máximo)
+//     b. El constructor solo debe pedir precio, color 
+//        y capacidad. 
+//     c. consumoEnergetico debe iniciar con valor de 100
+// ------------------------------------------------------------
+
+// ------------------------------------------------------------
+// 3.- Crea una subclase de Electrodomestico llamada Lavadora 
+//     con las siguientes características:
+//     a. Su atributo es carga(kg de ropa), además de los 
+//        atributos heredados.
+//     b. Crea el método precioFinal(). Este se calcula
+//        multiplicando el consumoEnergetico por la carga.
+// ------------------------------------------------------------
+
+
+// ------------------------------------------------------------
+// 4.- Crear una clase Bebida que herede a dos clases Cerveza 
+//     y Refresco. Ambas clases deben tener la propiedad 
+//     cantidad (ml). La clase Refresco debe tener el atributo
+//     azucar(g) y la clase Cerveza debe el atributo 
+//     porcentajeAlcohol. Crear los getters y setters
+//     correspondientes.
+// ------------------------------------------------------------
+
