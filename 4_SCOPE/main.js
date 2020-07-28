aa6d5ac081d4a7a14142877271931a5ad8652e97
hiromiakitao@gmail.com <hiromiakitao@gmail.com>
2020-07-27 22:09:07 -0500

    Scope y operador !!

---
+++ 4_SCOPE/main.js
@@ -0,0 +1,28 @@
+// function cinta() {
+//     const programa = "Cinta Roja";
+//     console.log(programa);
+//     return programa;
+// }
+
+// cinta();
+// let programa = cinta();
+// console.log(programa);
+
+
+let esMayor = true;
+// console.log(esMayor);
+
+// esMayor = false;
+
+// console.log(esMayor);
+
+// console.log(!esMayor);
+
+// console.log(!!esMayor);
+
+let number = '';
+console.log(!!number);
+
+if(number) {
+    console.log(number);
+}
